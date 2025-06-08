60eec84baffe2a568050f533a52d6b2a300d4b6f
Xin <admin@myflv.cn>
2025-06-08 21:00:27 +0800

    1

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -5,19 +5,18 @@
         <view class="content-view">
             <uv-form labelPosition="top" :model="form" labelWidth="256rpx" ref="formRef">
                 <uv-form-item label="执行命令">
-                    <uv-textarea v-model="form.cmd" />
+                    <uv-textarea v-model="form.cmd"/>
                 </uv-form-item>
                 
-                <uv-form-item label="输出结果"  >
+                <uv-form-item label="输出结果">
                     <uv-textarea v-model="form.result" disabled height="400rpx"/>
                 </uv-form-item>
             </uv-form>
             
             <uv-button type="primary" @click="execute">执行</uv-button>
         </view>
-        
-        
-      
+    
+    
     </view>
 </template>
 
@@ -25,29 +24,26 @@
 import {ref} from 'vue'
 import {exec} from 'kernelsu'
 
-const form = ref({
+const form = ref({})
 
-})
-
-const execute = async ()=>{
+const execute = async () => {
     const res = await exec(form.value.cmd)
-    
-    if (res.errno === 0){
-        form.value.result = res.stdout.replaceAll('\\n','\n')
-    }else {
-        form.value.result = res.stderr.replaceAll('\\n','\n')
+    if (res.errno === 0) {
+        form.value.result = res.stdout.replaceAll('\\n', '\n')
+    } else {
+        form.value.result = res.stderr.replaceAll('\\n', '\n')
     }
-    
 }
 </script>
 
 <style>
-.content-view{
+.content-view {
     flex: 1;
     padding: 24rpx;
     box-sizing: border-box;
 }
-.page-view{
+
+.page-view {
     width: 100%;
     height: 100%;
     display: flex;
